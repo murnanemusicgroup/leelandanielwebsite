5526444ebbf3c669de6c26dd968d0ac743c78764
leelandaniel <leeland.murnane1@icloud.com>
2025-06-10 18:47:14 -0400

    Create announce.js

---
+++ js/announce.js
@@ -0,0 +1,7 @@
+// Function to close the announcement popup
+function closeAnnouncementPopup() {
+    const popup = document.getElementById('announcementPopup');
+    if (popup) {
+        popup.style.display = 'none'; // Hide the popup by setting its display to 'none'
+    }
+}
