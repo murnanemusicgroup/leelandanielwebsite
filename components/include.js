128b43446232b6f7523707f0ac281a2cc132cffe
leelandaniel <213135120+leelandaniel@users.noreply.github.com>
2026-04-17 10:49:51 -0400

    getting older release day

--- components/include.js
+++ components/include.js
@@ -8,7 +8,6 @@ stylesheetLink.type = "text/css";
 stylesheetLink.title = "style";
 document.head.appendChild(stylesheetLink);
 
-
 const faviconLink = document.createElement("link");
 faviconLink.rel = "icon";
 faviconLink.type = "image/png";
@@ -18,24 +17,24 @@ document.head.appendChild(faviconLink);
 // hehehehe
 
 document.addEventListener("DOMContentLoaded", function () {
-	const includeHTML = (filePath, targetElementId) => {
-		fetch(filePath)
-			.then((response) => {
-				if (!response.ok) throw new Error(`Could not load ${filePath}`);
-				return response.text();
-			})
-			.then((data) => {
-				document.getElementById(targetElementId).innerHTML = data;
-			})
-			.catch((error) => console.error("Error including HTML:", error));
-	};
+  const includeHTML = (filePath, targetElementId) => {
+    fetch(filePath)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Could not load ${filePath}`);
+        return response.text();
+      })
+      .then((data) => {
+        document.getElementById(targetElementId).innerHTML = data;
+      })
+      .catch((error) => console.error("Error including HTML:", error));
+  };
 
-	includeHTML("/components/privacy.html", "privacy-placeholder");
-	includeHTML("/components/terms.html", "terms-placeholder");
-	includeHTML("/components/hero.html", "hero-placeholder");
-    includeHTML("/components/sign-up.html", "sign-up-placeholder");
-	includeHTML("/components/header.html", "header-placeholder");
-	includeHTML("/components/discography.html", "discography-placeholder");
-	includeHTML("/components/videos.html", "videos-placeholder");
-	includeHTML("/components/announcement.html", "announcement-placeholder");
+  includeHTML("/components/privacy.html", "privacy-placeholder");
+  includeHTML("/components/terms.html", "terms-placeholder");
+  includeHTML("/components/hero.html", "hero-placeholder");
+  includeHTML("/components/sign-up.html", "sign-up-placeholder");
+  includeHTML("/components/header.html", "header-placeholder");
+  includeHTML("/components/discography.html", "discography-placeholder");
+  includeHTML("/components/videos.html", "videos-placeholder");
+  includeHTML("/components/announcement.html", "announcement-placeholder");
 });
