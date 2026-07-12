2957e6a78a96aa060a88520701a10d0d700a7a75
leelandaniel <213135120+leelandaniel@users.noreply.github.com>
2026-07-12 12:08:37 -0400

    Update release date in countdownRedirect.js

--- js/countdownRedirect.js
+++ js/countdownRedirect.js
@@ -1,4 +1,4 @@
-const releaseDate = new Date("2025-09-07T12:00:00-04:00").getTime();
+const releaseDate = new Date("2026-07-13T00:00:00-04:00").getTime();
 
 if (new Date().getTime() < releaseDate) {
 	if (window.location.pathname.indexOf("/password") === -1) {
