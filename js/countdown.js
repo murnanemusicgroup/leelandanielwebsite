6373416f5824e230586503fcab2895b3faf0348c
leelandaniel <213135120+leelandaniel@users.noreply.github.com>
2026-07-12 12:07:31 -0400

    Update release date in countdown script

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const releaseDate = new Date("2026-03-06T00:00:00-04:00").getTime();
+	const releaseDate = new Date("2026-07-13T00:00:00-04:00").getTime();
 
 	if (new Date().getTime() >= releaseDate) {
 		window.location.href = "/";
